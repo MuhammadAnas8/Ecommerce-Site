8eb18611744528bf88421aa0f78e9fc4ef185fb7
MuhammadAnas8 <Muhammadanas.tech@gmail.com>
2024-06-30 00:30:57 +0500

    added Phones Page

--- src/assets/components/Pages/Phones/Phones.jsx
+++ src/assets/components/Pages/Phones/Phones.jsx
@@ -1,10 +1,20 @@
 import React from 'react'
+import Header from '../../Common/Header/Header'
+import ProductList from '../../Common/ProductList/ProductList'
+import PageHeader from '../Common/PageHeader/PageHeader'
+import  {PhonesProducts,PhonesProductsNewArrival,PhonesProductsTrennding}  from '../../Data/PhonesProducts'
 
 function Phones() {
   return (
     <div>
-      <h1>This is Phones page</h1>
-    </div>
+      <PageHeader PageName={'phones'} />
+      <Header heading="PHONES" subHeading="TRENDING NOW" />
+      <ProductList products={PhonesProductsTrennding} />
+      <Header heading="PHONES" subHeading="NEW ARRIVAL" />
+      <ProductList products={PhonesProductsNewArrival} />
+      <Header heading="ALL PRODUCTS" subHeading="PHONES" />
+      <ProductList products={PhonesProducts} />
+         </div>
   )
 }
 
